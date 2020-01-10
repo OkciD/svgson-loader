dc4b0fda62199f189aee97d63a411d77bcf1a6d3
Yury Baykov <y.baykov@corp.mail.ru>
2020-01-10 11:54:52 +0300

    tryin to update svgson

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const svgson = require('svgson');
 
 module.exports = function(content) {
 	const callback = this.async();
-	svgson(content, {
-		// uses default svgson cofig
-	}, function(result, err) {
-		if(err) return callback(err);
-		callback(null,
+
+	svgson.parse(content, {
+		compat: true,
+		camelCase: true
+	})
+		.then((result) => {
+			callback(null,
 `module.exports = ${JSON.stringify(result)};
 module.exports['default'] = module.exports;
 `
-		);
-	});
+			);
+		})
+		.catch((err) => {
+			callback(err);
+		})
 };
